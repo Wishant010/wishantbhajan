0debfd3304654bb9ac5c1a1c9538f78ef8050ded
Wishant010 <144028795+Wishant010@users.noreply.github.com>
2025-11-27 12:13:49 +0100

    alles goed in folders verplaats netjes en clean

--- src/pages-components/Homescreenpage/portfolio.tsx
+++ src/pages-components/Homescreenpage/portfolio.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React, { useState } from "react"
-import '../../components/animations.css'
 import { motion, AnimatePresence } from "framer-motion"
 import { useLanguage } from "../../contexts/LanguageContext"
 
