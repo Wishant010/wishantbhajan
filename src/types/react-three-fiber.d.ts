3516f88b6e2b1df6bd0a7e4986a9a626ba762b12
Wishant010 <144028795+Wishant010@users.noreply.github.com>
2025-12-18 02:05:09 +0100

    Build: update dist for deployment

--- src/types/react-three-fiber.d.ts
+++
@@ -1,9 +0,0 @@
-import { Object3DNode } from '@react-three/fiber';
-import { Mesh, PlaneGeometry } from 'three';
-
-declare module '@react-three/fiber' {
-  interface ThreeElements {
-    mesh: Object3DNode<Mesh, typeof Mesh>;
-    planeGeometry: Object3DNode<PlaneGeometry, typeof PlaneGeometry>;
-  }
-}
